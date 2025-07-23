981b1144b51af9c923ce2177cb3bcc04c8882e5c
Dhrubapramanik <pramanikdhruba180@gmail.com>
2025-07-23 19:44:32 +0530

    Product-Pricing-Component Complete

--- src/landing_page/pricing/PricingPage.jsx
+++ src/landing_page/pricing/PricingPage.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import Navbar from '../Navbar';
-import Footer from '../Footer';
 import Hero from './Hero';
 import Brokerage from './Brokerage';
+import OpenAccount from "../OpenAccount";
 
 function PricingPage() {
     return ( 
         <>
         <Hero />
+        <OpenAccount />
         <Brokerage />
         </>
      );
